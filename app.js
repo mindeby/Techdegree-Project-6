0ca2b8b1e195246842c43b48302d6dd1561822b5
mindeby <debora.marisa.ribeiro@gmail.com>
2019-05-14 10:05:29 +0200

    DOMContentLoaded

--- app.js
+++ app.js
@@ -1,51 +1,84 @@
-const qwerty = document.getElementById('qwerty');
-const phrase = document.getElementById('phrase');
-const missed = 0; //this variable will keep track of the number of guesses the player has missed. If 5 guesses missed he loses the game
-const phrases = [
-                  "GAME OF THRONES",
-                  "KILLING EVE",
-                  "STRANGER THINGS",
-                  "BLACK MIRROR",
-                  "PATRIOT",
-                  "SHAMELESS",
-                  "WESTWORLD",
-                  "THIS IS US",
-                  "ORANGE IS THE NEW BLACK",
-                  "BIG LITTLE LIES"
-                ];
+document.addEventListener('DOMContentLoaded', () => {
 
 
-overlay.addEventListener('click', (event) => {
-  if(event.target.className === "btn__reset") {
-    overlay.style.display = "none";
-  }
-});
+    const qwerty = document.getElementById('qwerty');
+    const phrase = document.getElementById('phrase');
+    const missed = 0; //this variable will keep track of the number of guesses the player has missed. If 5 guesses missed he loses the game
+    const phrases = [
+                      "GAME OF THRONES",
+                      "KILLING EVE",
+                      "STRANGER THINGS",
+                      "BLACK MIRROR",
+                      "PATRIOT",
+                      "SHAMELESS",
+                      "WESTWORLD",
+                      "THIS IS US",
+                      "ORANGE IS THE NEW BLACK",
+                      "BIG LITTLE LIES"
+                    ];
+    const lettersArray = document.getElementsByClassName('letter');
 
-function getRandomPhraseAsArray(arr) {
-  const randomPhrase = arr[Math.floor(Math.random()* arr.length)];
-  const splitPhrase = randomPhrase.split("");
-  return splitPhrase;
-}
 
-const phraseArray = getRandomPhraseAsArray(phrases);
+    overlay.addEventListener('click', (event) => {
+      if(event.target.className === "btn__reset") {
+        overlay.style.display = "none";
+      }
+    });
 
-function addPhraseToDisplay(arr){
-  for (i=0; i < arr.length; i+=1) {
-    const li = document.createElement('li');
-    const ul = document.getElementById('phrase').querySelector('ul');
-    console.log("Info below");
-    li.append(arr[i]);
-    console.log([i]);
-    ul.appendChild(li);
-    console.log(ul);
-    if (li.textContent !== " "){
-      li.classList.add("letter");
+    function getRandomPhraseAsArray(arr) {
+      const randomPhrase = arr[Math.floor(Math.random()* arr.length)];
+      const splitPhrase = randomPhrase.split("");
+      return splitPhrase;
     }
-  }
-}
 
-addPhraseToDisplay(phraseArray);
+    const phraseArray = getRandomPhraseAsArray(phrases);
 
-function checkLetter (letterButton){
+    function addPhraseToDisplay(arr){
+      for (i=0; i < arr.length; i+=1) {
+        const li = document.createElement('li');
+        const ul = document.getElementById('phrase').querySelector('ul');
+        li.append(arr[i]);
+        ul.appendChild(li);
+        if (li.textContent !== " "){
+          li.classList.add("letter");
+        }
+      }
+    }
 
-}
+    const guessedLetters =[];
+
+    addPhraseToDisplay(phraseArray);
+
+
+    console.log(lettersArray.length);
+
+    function checkLetter (keyboardButton){
+
+        console.log("LETTER ARRAY is")
+        console.log(lettersArray[i].textContent)
+        if (lettersArray[i].textContent === keyboardButton.textContent.toUpperCase()){
+          lettersArray[i].classList.add("show");
+          guessedLetters.push([i]);
+          return lettersArray[i];
+        } else {
+          return null;
+        }
+    }
+
+
+    qwerty.addEventListener('click', (event) => {
+      if (event.target.tagName === 'BUTTON') {
+        for (i=0; i < (lettersArray.length); i += 1) {
+        event.target.classList.add("chosen");
+        console.log("was a button pushed?");
+        console.log(event.target.textContent);
+        var letterFound = checkLetter(event.target);
+        console.log("logs letter found?");
+        console.log(letterFound);
+        }
+      }
+    });
+
+
+
+});
